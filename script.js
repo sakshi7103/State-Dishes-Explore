8497732ab47202e26203a9fa6f35a4ec6d767077
SAKSHI BHALALA <sakshibhalala710@gmail.com>
2025-02-09 22:16:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,175 @@
+const dishesData={
+    gujarat:[
+        {
+            name: "Khaman",
+            description:"Khaman is a popular Gujarati snack made from steamed gram flour, spiced with mustard seeds, and garnished with coriander.",
+            image:"image/gujarati/dhokla.jpg"
+        },
+        {
+            name: "Fafda-Jalebi",
+            description:"Fafda Jalebi is a traditional Gujarati breakfast, combining crispy, salty fafda and sweet, crispy jalebi, often enjoyed with a side of spicy chutney.",
+            image:"image/gujarati/fafda.jpg"
+        },
+        {
+            name: "Handvo",
+            description:"Handvo is a savory, spiced cake made from rice, lentils, and vegetables. It’s baked or steamed, offering a flavorful, nutritious snack or meal.",
+            image:"image/gujarati/handvo.jpg"
+        },
+        {
+            name: "Fruit-Salad",
+            description:"Fruit salad is a mix of fresh, chopped fruits like apples, bananas, and berries. It’s a healthy, refreshing, and colorful dish, often served as a snack.",
+            image:"image/gujarati/fruitsalad.jpg"
+        },
+        {
+            name: "Undhiyu",
+            description:"Undhiyu is a traditional Gujarati dish made with mixed vegetables, beans, and spices, cooked in a pot. It's flavorful, hearty, and often enjoyed during festivals.",
+            image:"image/gujarati/undhiyu.jpg"
+        },
+        {
+            name: "Rotlo",
+            description:"Rotlo is a traditional Gujarati flatbread made from whole wheat or millet flour. It's thick, hearty, and commonly served with vegetables or yogurt.",
+            image:"image/gujarati/rotlo.jpg"
+        },
+        {
+            name: "Ghooghra",
+            description:"Googra is a popular Gujarati sweet made from wheat flour, sugar, and ghee, shaped into dumplings, and fried until golden. It’s delicious and often served during festivals.",
+            image:"image/gujarati/googra.jpg"
+        },
+        {
+            name: "Mohanthal",
+            description:"Mohanthal is a traditional Gujarati sweet made from gram flour, ghee, sugar, and cardamom. It’s a rich, dense, and delicious dessert enjoyed during festivals.",
+            image:"image/gujarati/mohanthal.jpg"
+        },
+    ],
+    maharastra:[
+            {
+                name: "Kothimbir-Vadi",
+                description:"Kothimbir Vadi is a popular Maharashtrian snack made with coriander, chickpea flour, and spices. It’s steamed or fried into crispy, flavorful bite-sized pieces.",
+                image:"image/maharastra/vadi.jpg"
+            },
+            {
+                name: "Misal-Pav",
+                description:"Misal Pav is a famous Maharashtrian dish made of spicy curry (misal) with sprouts, served with soft bread rolls (pav). It’s flavorful and filling.",
+                image:"image/maharastra/misal.jpg"
+            },
+            {
+                name: "Modak",
+                description:"Modak is a sweet dumpling made from rice flour, filled with coconut and jaggery. It’s traditionally offered during Ganesh Chaturthi and loved for its delicious taste.",
+                image:"image/maharastra/modak.jpg"
+            },
+            {
+                name: "Pav-Bhaji",
+                description:"Pav Bhaji is a popular Mumbai street food made of mashed vegetables cooked in spices, served with buttered bread rolls (pav). It’s flavorful and filling.",
+                image:"image/maharastra/pavbhaji.jpg"
+            },
+            {
+                name: "Poha",
+                description:"Poha is a light, savory dish made from flattened rice, cooked with onions, mustard seeds, curry leaves, and spices. It’s a popular breakfast in India.",
+                image:"image/maharastra/poha.jpg"
+            },
+            {
+                name: "PuranPoli",
+                description:"Puran Poli is a sweet Indian flatbread stuffed with a filling of jaggery and lentils. It’s a traditional dish, often made during festivals.",
+                image:"image/maharastra/puranpoli.jpg"
+            },
+            {
+                name: "Vadapav",
+                description:"Vada Pav is a popular Mumbai street food, consisting of a spicy potato fritter (vada) served in a soft bread roll (pav). It’s flavorful and filling.",
+                image:"image/maharastra/vadapav.jpg"
+            },
+    ],
+    punjab:[
+        {
+            name: "Chole",
+            description:"Chole is a popular Punjabi dish made of spicy chickpeas cooked with onions, tomatoes, and spices. It’s often served with rice or fried bread (bhature).",
+            image:"image/punjab/chole.jpg"
+        },
+        {
+            name: "Kada-Prasad",
+            description:"Kadha Prasad is a traditional Punjabi sweet made from wheat flour, sugar, ghee, and water. It’s often served as prasad in Gurudwaras",
+            image:"image/punjab/kadaprasad.jpg"
+        },
+        {
+            name: "Lassi",
+            description:"Lassi is a traditional Punjabi drink made from yogurt, water, and spices. It can be sweet or salty and is refreshing, often enjoyed in hot weather.",
+            image:"image/punjab/lassi.jpg"
+        },
+        {
+            name: "Makai-Roti",
+            description:"Makai Roti is a traditional Punjabi flatbread made from cornmeal. It’s usually served with butter, mustard greens (sarson da saag), and chutney",
+            image:"image/punjab/makairoti.jpg"
+        },
+        {
+            name: "Dal-Makhani",
+            description:"Dal Makhani is a popular Punjabi dish made from black lentils and kidney beans, cooked in a creamy, spiced gravy. It’s rich and flavorful.",
+            image:"image/punjab/makhani.jpg"
+        },
+        {
+            name: "Paratha",
+            description:"Paratha is a popular Punjabi flatbread made from whole wheat flour, often stuffed with fillings like potatoes, paneer, or spinach. It’s usually served with yogurt or pickle.",
+            image:"image/punjab/paratha.jpg"
+        },
+    ],
+    rajasthan:[
+        {
+            name: "Dal-Bati",
+            description:"Dal Baati is a traditional Rajasthani dish, consisting of baked round bread (baati) served with spicy lentil soup (dal). It’s flavorful and filling.",
+            image:"image/rajasthan/dalbati.jpg"
+        },
+        {
+            name: "Gatte-Ki-Sabji",
+            description:"Gatte ki Sabji is a Rajasthani curry made from gram flour dumplings (gatte) cooked in a spiced yogurt-based gravy. It’s flavorful and served with rice or roti.",
+            image:"image/rajasthan/gatte.jpg"
+        },
+        {
+            name: "Ghevar",
+            description:"Ghevar is a traditional Rajasthani sweet made from flour, ghee, and sugar syrup. It’s crispy, soaked in sweetness, and often enjoyed during festivals like Teej and Raksha Bandhan.",
+            image:"image/rajasthan/ghevar.jpg"
+        },
+        {
+            name: "Kachori",
+            description:"Kachori is a deep-fried, round pastry filled with spiced lentils or potatoes. It’s a popular snack in India, often served with chutney.",
+            image:"image/rajasthan/kachori.jpg"
+        },
+        {
+            name: "Malpua",
+            description:"Malpua is a sweet, deep-fried Indian pancake made from flour, sugar, and coconut. It’s often served with rabri (sweet milk) during festivals.",
+            image:"image/rajasthan/malpua.jpg"
+        },
+        {
+            name: "Mirchi-Vada",
+            description:"Mirchi Vada is a popular Rajasthani snack made by stuffing green chilies with spiced potato filling, coating them in batter, and deep-frying until crispy.",
+            image:"image/rajasthan/mirchivada.jpg"
+        },
+    ]
+}
+function showDish(state){
+    const dishes=document.getElementById('dishes');
+    dishes.innerHTML="";
+
+    if(dishesData[state]){
+        dishesData[state].forEach(dish => {
+            const dishcard=document.createElement('div');
+            dishcard.classList.add('dish-card');
+
+            const dishImage=document.createElement('img');
+            dishImage.src=dish.image;
+            dishImage.alt=dish.name;
+
+            dishImage.classList.add('dish-image');
+
+            const dishName=document.createElement('h3');
+            dishName.textContent=dish.name;
+
+            const dishDescription=document.createElement('p');
+            dishDescription.textContent=dish.description;
+
+            dishcard.appendChild(dishImage);
+            dishcard.appendChild(dishName);
+
+            dishcard.appendChild(dishDescription);
+
+            dishes.appendChild(dishcard);
+        });
+    }
+}
